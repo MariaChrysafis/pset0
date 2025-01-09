97ce724f2a24b216b780639d4abf46aa1e8efc26
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2025-01-08 22:52:15 -0500

    pset0

---
+++ ps0-main/README.md
@@ -0,0 +1,4 @@
+### Problem Set 0: Turtle Graphics
+
+Students in 6.102: do not clone this read-only repo.
+[Follow problem set instructions to create your own ps0 repo with Didit.](https://web.mit.edu/6.102/www/sp23/psets/ps0/)
---
+++ ps0-main/src/turtlesoup.ts
@@ -0,0 +1,101 @@
+/* Copyright (c) 2007-2023 MIT 6.102/6.031/6.005 course staff, all rights reserved.
+ * Redistribution of original or derived work requires permission of course staff.
+ */
+
+import fs from 'fs';
+import open from 'open';
+import { Turtle, DrawableTurtle, LineSegment, PenColor, Point } from './turtle.js';
+
+/**
+ * Draw a square.
+ * 
+ * @param turtle the turtle context
+ * @param sideLength length of each side, must be >= 0
+ */
+export function drawSquare(turtle: Turtle, sideLength: number): void {
+    throw new Error("implement me!");
+}
+
+/**
+ * Determine the length of a chord of a circle.
+ * (There is a simple formula; derive it or look it up.)
+ * 
+ * @param radius radius of a circle, must be > 0
+ * @param angle in radians, where 0 <= angle < Math.PI
+ * @returns the length of the chord subtended by the given `angle` 
+ *          in a circle of the given `radius`
+ */
+export function chordLength(radius: number, angle: number): number {
+    throw new Error("implement me!");
+}
+
+/**
+ * Approximate a circle by drawing a many-sided regular polygon, 
+ * using exactly `numSides` small counterclockwise turns,
+ * so that the turtle is back to its original heading and position
+ * after the drawing is complete.
+ * 
+ * @param turtle the turtle context
+ * @param radius radius of the circle circumscribed around the polygon, must be > 0
+ * @param numSides number of sides of the polygon to draw, must be >= 10
+ */
+export function drawApproximateCircle(turtle: Turtle, radius: number, numSides: number): void {
+    throw new Error("implement me!");
+}
+
+/**
+ * Calculate the distance between two points.
+ * 
+ * @param p1 one point
+ * @param p2 another point
+ * @returns Euclidean distance between p1 and p2
+ */
+export function distance(p1: Point, p2: Point): number {
+    throw new Error("implement me!");
+}
+
+/**
+ * Given a list of points, find a sequence of turns and moves that visits the points in order,
+ * ending with the turtle facing its original heading.
+ * 
+ * @param points array of N input points.  Adjacent points must be distinct, and the array must not start with (0,0).
+ * @returns an array of length 2N+1 of the form [turn_0, move_0, ..., turn_N-1, move_N-1, turn_N]
+ *    such that if the turtle starts at (0,0) heading up (positive y direction), 
+ *    and executes turn(turn_i) and forward(move_i) actions in the same order, 
+ *    then it will be at points[i] after move_i for all valid i,
+ *    and be back to its original upward heading after turn_N.
+ */
+export function findPath(points: Array<Point>): Array<number> {
+    throw new Error("Implement me!");
+}
+
+/**
+ * Draw your personal, custom art.
+ * 
+ * Many interesting images can be drawn using the simple implementation of a turtle.
+ * See the problem set handout for more information.
+ * 
+ * @param turtle the turtle context
+ */
+export function drawPersonalArt(turtle: Turtle): void {
+    throw new Error("Implement me!");
+}
+
+/**
+ * Main program.
+ * 
+ * This function creates a turtle and draws in a window.
+ */
+export function main(): void {
+    const turtle: Turtle = new DrawableTurtle();
+
+    const sideLength = 40;
+    // drawSquare(turtle, sideLength);
+
+    // draw into a file
+    const svgDrawing = turtle.getSVG();
+    fs.writeFileSync('output.html', `<html>\n${svgDrawing}</html>\n`);
+
+    // open it in a web browser
+    void open('output.html');
+}
